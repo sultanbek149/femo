755e88cd55b00b9b2645e1b228a32e5371054797
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2025-04-15 05:24:22 +0500

    show pass

---
+++ src/scripts/showpass.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+    const passwordField = document.getElementById('password');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+}
