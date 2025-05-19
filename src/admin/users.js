e2a51f006db7d1f9a897aec2df04898d84f01389
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2025-05-19 18:20:56 +0500

    final2

--- src/admin/users.js
+++ src/admin/users.js
@@ -201,12 +201,12 @@ function renderUsers(users) {
         <td class="px-6 py-4 text-sm whitespace-nowrap">
           <!-- Кнопки действий остаются без изменений -->
             <div class="flex justify-between gap-2 *:cursor-pointer">
-            <button class="text-gray-400 hover:text-red-500">
+            <button type="button" onclick="toggleModal('modalDel')" class="text-gray-400 hover:text-red-500">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
               </svg>
             </button>
-            <button class="hover:text-blue-primary text-gray-400">
+            <button type="button" onclick="toggleModal('modalEdit')" class="hover:text-blue-primary text-gray-400">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
               </svg>
